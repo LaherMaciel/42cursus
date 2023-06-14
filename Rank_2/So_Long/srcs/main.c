d822b5620987cace7340bf62b5db1b057dc123be
LaherMaciel <Laherwpayotmaciel@gmail.com>
2023-06-14 23:25:14 +0100

    Day 12. 2 commit

--- Rank_2/So_Long/srcs/main.c
+++ Rank_2/So_Long/srcs/main.c
@@ -6,38 +6,47 @@
 /*   By: lwencesl <lwencesl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 15:19:10 by lwencesl          #+#    #+#             */
-/*   Updated: 2023/06/07 22:37:53 by lwencesl         ###   ########.fr       */
+/*   Updated: 2023/06/14 10:42:48 by lwencesl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	window_start(t_win win)
+/**
+ * @brief creat the map, initialize the window, put
+ * the first image on the window.
+ *
+ * @param win
+ */
+void	window_start(t_win *win)
 {
+	int		test;
+	t_win	init;
 	t_data	img;
 	int		i;
 
-	win.mapa = creat_map_mod();
+	init.mapa = creat_map_mod();
 	//win.mapa = creat_map();
-	if (win.mapa == NULL)
+	if (init.mapa == NULL)
 		return ;
-	if (map_base_check(win) == 0)
+	if (map_base_check(init) == 0)
 	{
 		ft_printf("ERROR\n");
 		return ;
 	}
-	ft_printf("%s\n\n", win.mapa[0]);
-	win = window_init(win);
-	img = create_image(win, player_image(&win));
-	mlx_hook(win.mlx_win, 2, 1L<<0, keycode_decisions, &win);
-	mlx_hook(win.mlx_win, 17, 1L, window_destroy, &win);
-	win.mapa = win.mapa;
-	mlx_loop(win.mlx);
+	ft_printf("%s\n\n", init.mapa[0]);
+	init = window_init(init);
+	*win = init;
+	img = create_image(win, player_image(win));
+	mlx_hook(win->mlx_win, 2, 1L<<0, keycode_decisions, win);
+	mlx_hook(win->mlx_win, 17, 1L, window_destroy, win);
+	win->mapa = win->mapa;
+	mlx_loop(win->mlx);
 	ft_printf("img: %p\n", img);
 	i = -1;
-	while (win.mapa[++i])
-		free(win.mapa[i]);
-	free(win.mapa);
+	while (win->mapa[++i])
+		free(win->mapa[i]);
+	free(win->mapa);
 }
 
 int	main(void)
@@ -50,7 +59,7 @@ int	main(void)
 	win.heigth_size = 1080;
 	win.image_heigth = 64;
 	win.image_length = 64;
-	window_start(win);
+	window_start(&win);
 	return (0);
 }
 
